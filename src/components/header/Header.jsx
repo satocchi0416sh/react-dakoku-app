a3cef80f066fbb07007c34642a1416d19894a5f4
satocchi0416sh <satoyoshi44@gmail.com>
2022-01-10 16:11:25 +0900

    add login button to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import { Google } from '@mui/icons-material'
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 const Header = ({ page, changePage }) => {
@@ -22,8 +22,9 @@ const Header = ({ page, changePage }) => {
                             <Google />
                         </IconButton>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Arklet 入退室アプリ - {page}
+                            Arklet 入退室アプリ - <Typography component={"span"} variant="body1">{page}</Typography>
                         </Typography>
+                        <Button onClick={() => history.push("/login")} color="inherit">ログイン</Button>
                     </Toolbar>
                 </AppBar>
             </Box>
